e84b9c042c5336b8d9823e14f7cbf99a95595261
vinitha3008 <vinaivinitha@gmail.com>
2025-01-20 18:21:27 +0530

    Add files via upload
    Day1_Assignment

---
+++ Chrome.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		
+		System.out.println("version-"+version);
+		System.out.println("developer-"+developer);
+		System.out.println("isBeta-"+isBeta);
+		System.out.println("releaseYear-"+releaseYear);
+		System.out.println("shortcutKey-"+shortcutKey);
+
+	}
+
+}
